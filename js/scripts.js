2f78d617a8bf2f944f418fbb35459ff4128eb681
Cameron Anderson and Kayla Ondracek <cameron@>
2018-01-17 10:10:17 -0800

    add logic to select best response based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
   $("#fortune").submit(function(event) {
     event.preventDefault();
-    
+
+    var lucky = 0;
+    var unlucky = 0;
+
+    $("input:checkbox:checked").each(function() {
+      if ($(this).val() === "lucky") {
+        lucky += 1;
+      } else {
+        unlucky += 1;
+      }
+    });
+    $(".result").hide();
+
+    if (lucky - 1 > unlucky) {
+      $("#lucky").show();
+    } else if (unlucky - 1 > lucky) {
+      $("#unlucky").show();
+    } else {
+      $("#whatever").show();
+    }
+
   });
 
 });
